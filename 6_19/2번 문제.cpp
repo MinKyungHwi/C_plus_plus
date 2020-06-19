19be69c49f9058cb812f746ecb5503b13332569f
MinKyungHwi <49218251+MinKyungHwi@users.noreply.github.com>
2020-06-19 23:04:17 +0900

    6_19
    Add

---
+++ 6_19/2번 문제.cpp
@@ -0,0 +1,17 @@
+/*
+#include <iostream>
+using namespace std;
+
+int  half(double& n1) {
+	int result;
+
+	result = n1 / 2;
+	return result;
+}
+
+int main() {
+	double n = 20;
+	n = half(n);
+	cout << n;
+}
+*/
---
+++ 6_19/3번 문제.cpp
@@ -0,0 +1,24 @@
+/*
+#include <iostream>
+using namespace std;
+
+string cobine(string& s1, string& s2, string s3) {
+	string sum;
+
+	sum = s1 + s2;
+	s3 = sum;
+	return s3;
+}
+
+
+int main() {
+	string text1("I love you"), text2(" very much");
+	string text3;
+
+	text3 = cobine(text1, text2, text3);
+
+
+	cout << text3;
+
+}
+*/
