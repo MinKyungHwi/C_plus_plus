6247cce5b1fe11281ae4617db0f26e785ceed1a5
MinKyungHwi <49218251+MinKyungHwi@users.noreply.github.com>
2020-06-23 20:52:47 +0900

    6_23 ADD
    commit

---
+++ 6_23/Thread_Practice.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+
+void func1() {
+	for (int i = 0; i < 10; i++) {
+		cout << "쓰레드 1 작동중! \n";
+	}
+}
+
+void func2() {
+	for (int i = 0; i < 10; i++) {
+		cout << "쓰레드 2 작동중! \n";
+	}
+}
+
+void func3() {
+	for (int i = 0; i < 10; i++) {
+		cout << "쓰레드 3 작동중! \n";
+	}
+}
+	thread t1(func1);
+	thread t2(func2);
+	thread t3(func3);
+
+	
+int main() {
+	t1.join();
+	t2.join();
+	t3.join();
+}
